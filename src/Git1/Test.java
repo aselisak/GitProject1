a2561b176e838254061dacb3e181edf3cb5b38f9
aselisak <kiyatova1984@gmail.com>
2022-05-14 12:01:59 -0400

    3rd commit

--- src/Git1/Test.java
+++ src/Git1/Test.java
@@ -3,5 +3,11 @@ package Git1;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Asel");
+
+        int x=0;
+        while (x<10){
+            x++;
+            System.out.println(x);
+        }
     }
 }
