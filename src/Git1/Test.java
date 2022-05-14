7e67d78ebd29da1694f12993bc44fa631aee8fc3
aselisak <kiyatova1984@gmail.com>
2022-05-14 12:46:10 -0400

    5th com

--- src/Git1/Test.java
+++ src/Git1/Test.java
@@ -4,9 +4,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Asel");
 
-        int x=0;
-        while (x<10){
-            x++;
+        int x=10;
+        while (x>0){
+            x--;
             System.out.println(x);
         }
         System.out.println("i am not correct code");
