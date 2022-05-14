52b70c3a5ad8b1f74ece389e93ea5b455964e864
aselisak <kiyatova1984@gmail.com>
2022-05-14 15:04:14 -0400

    123

--- src/Git1/Test.java
+++ src/Git1/Test.java
@@ -1,8 +1,11 @@
 package Git1;
 
+import com.sun.xml.internal.ws.encoding.soap.SOAP12Constants;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Asel");
+        System.out.println("Im am groot");
+
 
         int x=10;
         while (x>0){
@@ -11,5 +14,6 @@ public class Test {
         }
         System.out.println("i am not correct code");
         System.out.println("I am correct code");
+        System.out.println("hellooo my friend");
     }
 }
